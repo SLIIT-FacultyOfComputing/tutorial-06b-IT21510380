d2e300fd4c422d4774c1217cd6295964a9fc9cfc
it21510380 <87371820+IT21510380@users.noreply.github.com>
2021-09-04 07:29:38 +0000

    completed

--- Student.h
+++ Student.h
@@ -1,9 +1,15 @@
 class Student {
   // private section
+
+  private :
   //    int studentId
   //    name <- 20 charcters
   
   // public section
+
+  public :
   //    assignDetails() method declaration
+        void assignDetails(int sId , char sname[]);
   //    display() method declaration
+        void display();
 };
